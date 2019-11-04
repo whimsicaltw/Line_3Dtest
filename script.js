8a1f6120291019bd7f45dec77c08b8f832aa45a8
whimsicaltw <31072711+whimsicaltw@users.noreply.github.com>
2019-11-04 19:32:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/OrbitControls.js';
 import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/loaders/GLTFLoader.js';
+import {GUI} from 'https://threejsfundamentals.org/threejs/../3rdparty/dat.gui.module.js';
 
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.shadowMap.enabled = true;
 
   const fov = 45;
   const aspect = 2;  // the canvas default
@@ -18,7 +20,7 @@ function main() {
   controls.update();
 
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color('black');
+  scene.background = new THREE.Color('#DEFEFF');
 
   {
     const planeSize = 40;
@@ -53,9 +55,124 @@ function main() {
     const color = 0xFFFFFF;
     const intensity = 1;
     const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(5, 10, 2);
+    light.castShadow = true;
+    light.position.set(-250, 800, -850);
+    light.target.position.set(-550, 40, -450);
+
+    light.shadow.bias = -0.004;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+
     scene.add(light);
     scene.add(light.target);
+    const cam = light.shadow.camera;
+    cam.near = 1;
+    cam.far = 2000;
+    cam.left = -1500;
+    cam.right = 1500;
+    cam.top = 1500;
+    cam.bottom = -1500;
+
+    const cameraHelper = new THREE.CameraHelper(cam);
+    scene.add(cameraHelper);
+    cameraHelper.visible = false;
+    const helper = new THREE.DirectionalLightHelper(light, 100);
+    scene.add(helper);
+    helper.visible = false;
+
+    function makeXYZGUI(gui, vector3, name, onChangeFn) {
+      const folder = gui.addFolder(name);
+      folder.add(vector3, 'x', vector3.x - 500, vector3.x + 500).onChange(onChangeFn);
+      folder.add(vector3, 'y', vector3.y - 500, vector3.y + 500).onChange(onChangeFn);
+      folder.add(vector3, 'z', vector3.z - 500, vector3.z + 500).onChange(onChangeFn);
+      folder.open();
+    }
+
+    function updateCamera() {
+      // update the light target's matrixWorld because it's needed by the helper
+      light.updateMatrixWorld();
+      light.target.updateMatrixWorld();
+      helper.update();
+      // update the light's shadow camera's projection matrix
+      light.shadow.camera.updateProjectionMatrix();
+      // and now update the camera helper we're using to show the light's shadow camera
+      cameraHelper.update();
+    }
+    updateCamera();
+
+    class DimensionGUIHelper {
+      constructor(obj, minProp, maxProp) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+      }
+      get value() {
+        return this.obj[this.maxProp] * 2;
+      }
+      set value(v) {
+        this.obj[this.maxProp] = v /  2;
+        this.obj[this.minProp] = v / -2;
+      }
+    }
+
+    class MinMaxGUIHelper {
+      constructor(obj, minProp, maxProp, minDif) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+        this.minDif = minDif;
+      }
+      get min() {
+        return this.obj[this.minProp];
+      }
+      set min(v) {
+        this.obj[this.minProp] = v;
+        this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+      }
+      get max() {
+        return this.obj[this.maxProp];
+      }
+      set max(v) {
+        this.obj[this.maxProp] = v;
+        this.min = this.min;  // this will call the min setter
+      }
+    }
+
+    class VisibleGUIHelper {
+      constructor(...objects) {
+        this.objects = [...objects];
+      }
+      get value() {
+        return this.objects[0].visible;
+      }
+      set value(v) {
+        this.objects.forEach((obj) => {
+          obj.visible = v;
+        });
+      }
+    }
+
+    const gui = new GUI();
+    gui.close();
+    gui.add(new VisibleGUIHelper(helper, cameraHelper), 'value').name('show helpers');
+    gui.add(light.shadow, 'bias', -0.1, 0.1, 0.001);
+    {
+      const folder = gui.addFolder('Shadow Camera');
+      folder.open();
+      folder.add(new DimensionGUIHelper(light.shadow.camera, 'left', 'right'), 'value', 1, 4000)
+        .name('width')
+        .onChange(updateCamera);
+      folder.add(new DimensionGUIHelper(light.shadow.camera, 'bottom', 'top'), 'value', 1, 4000 )
+        .name('height')
+        .onChange(updateCamera);
+      const minMaxGUIHelper = new MinMaxGUIHelper(light.shadow.camera, 'near', 'far', 0.1);
+      folder.add(minMaxGUIHelper, 'min', 1, 1000, 1).name('near').onChange(updateCamera);
+      folder.add(minMaxGUIHelper, 'max', 1, 4000, 1).name('far').onChange(updateCamera);
+      folder.add(light.shadow.camera, 'zoom', 0.01, 1.5, 0.01).onChange(updateCamera);
+    }
+
+    makeXYZGUI(gui, light.position, 'position', updateCamera);
+    makeXYZGUI(gui, light.target.position, 'target', updateCamera);
   }
 
   function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
@@ -84,11 +201,89 @@ function main() {
     camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
   }
 
+  let curve;
+  let curveObject;
+  {
+    const controlPoints = [
+      [1.118281, 5.115846, -3.681386],
+      [3.948875, 5.115846, -3.641834],
+      [3.960072, 5.115846, -0.240352],
+      [3.985447, 5.115846, 4.585005],
+      [-3.793631, 5.115846, 4.585006],
+      [-3.826839, 5.115846, -14.736200],
+      [-14.542292, 5.115846, -14.765865],
+      [-14.520929, 5.115846, -3.627002],
+      [-5.452815, 5.115846, -3.634418],
+      [-5.467251, 5.115846, 4.549161],
+      [-13.266233, 5.115846, 4.567083],
+      [-13.250067, 5.115846, -13.499271],
+      [4.081842, 5.115846, -13.435463],
+      [4.125436, 5.115846, -5.334928],
+      [-14.521364, 5.115846, -5.239871],
+      [-14.510466, 5.115846, 5.486727],
+      [5.745666, 5.115846, 5.510492],
+      [5.787942, 5.115846, -14.728308],
+      [-5.423720, 5.115846, -14.761919],
+      [-5.373599, 5.115846, -3.704133],
+      [1.004861, 5.115846, -3.641834],
+    ];
+    const p0 = new THREE.Vector3();
+    const p1 = new THREE.Vector3();
+    curve = new THREE.CatmullRomCurve3(
+      controlPoints.map((p, ndx) => {
+        p0.set(...p);
+        p1.set(...controlPoints[(ndx + 1) % controlPoints.length]);
+        return [
+          (new THREE.Vector3()).copy(p0),
+          (new THREE.Vector3()).lerpVectors(p0, p1, 0.1),
+          (new THREE.Vector3()).lerpVectors(p0, p1, 0.9),
+        ];
+      }).flat(),
+      true,
+    );
+    {
+      const points = curve.getPoints(250);
+      const geometry = new THREE.BufferGeometry().setFromPoints(points);
+      const material = new THREE.LineBasicMaterial({color: 0xff0000});
+      curveObject = new THREE.Line(geometry, material);
+      curveObject.scale.set(100, 100, 100);
+      curveObject.position.y = -621;
+      curveObject.visible = false;
+      scene.add(curveObject);
+    }
+  }
+
+  const cars = [];
   {
     const gltfLoader = new GLTFLoader();
     gltfLoader.load('https://raw.githubusercontent.com/whimsicaltw/Line_3Dtest/master/model/test1.gltf', (gltf) => {
       const root = gltf.scene;
       scene.add(root);
+
+      root.traverse((obj) => {
+        if (obj.castShadow !== undefined) {
+          obj.castShadow = true;
+          obj.receiveShadow = true;
+        }
+      });
+
+      const loadedCars = root.getObjectByName('Cars');
+      const fixes = [
+        { prefix: 'Car_08', y: 0,  rot: [Math.PI * .5, 0, Math.PI * .5], },
+        { prefix: 'CAR_03', y: 33, rot: [0, Math.PI, 0], },
+        { prefix: 'Car_04', y: 40, rot: [0, Math.PI, 0], },
+      ];
+
+      root.updateMatrixWorld();
+      for (const car of loadedCars.children.slice()) {
+        const fix = fixes.find(fix => car.name.startsWith(fix.prefix));
+        const obj = new THREE.Object3D();
+        car.position.set(0, fix.y, 0);
+        car.rotation.set(...fix.rot);
+        obj.add(car);
+        scene.add(obj);
+        cars.push(obj);
+      }
 
       // compute the box that contains all the stuff
       // from root and below
@@ -118,11 +313,42 @@ function main() {
     return needResize;
   }
 
-  function render() {
+  // create 2 Vector3s we can use for path calculations
+  const carPosition = new THREE.Vector3();
+  const carTarget = new THREE.Vector3();
+
+  function render(time) {
+    time *= 0.001;  // convert to seconds
+
     if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
+    }
+
+    {
+      const pathTime = time * .01;
+      const targetOffset = 0.01;
+      cars.forEach((car, ndx) => {
+        // a number between 0 and 1 to evenly space the cars
+        const u = pathTime + ndx / cars.length;
+
+        // get the first point
+        curve.getPointAt(u % 1, carPosition);
+        carPosition.applyMatrix4(curveObject.matrixWorld);
+
+        // get a second point slightly further down the curve
+        curve.getPointAt((u + targetOffset) % 1, carTarget);
+        carTarget.applyMatrix4(curveObject.matrixWorld);
+
+        // put the car at the first point (temporarily)
+        car.position.copy(carPosition);
+        // point the car the second point
+        car.lookAt(carTarget);
+
+        // put the car between the 2 points
+        car.position.lerpVectors(carPosition, carTarget, 0.5);
+      });
     }
 
     renderer.render(scene, camera);
