003988b61145863f156628ba23d3ce92de2f98a1
whimsicaltw <31072711+whimsicaltw@users.noreply.github.com>
2019-11-04 21:57:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function main() {
   {
     const skyColor = 0xB1E1FF;  // light blue
     const groundColor = 0xB97A20;  // brownish orange
-    const intensity = 1.2;
+    const intensity = 1.5;
     const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
     scene.add(light);
   }
